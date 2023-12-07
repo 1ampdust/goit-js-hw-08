0e03860df4019ef66779239177177fc927f7d9ae
Lampdust <lampdust06@gmail.com>
2023-12-08 00:46:01 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(handleInput, 500));
+
+window.addEventListener('load', populateFormFields);
+
+form.addEventListener('submit', handleSubmit);
+
+function handleInput() {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function populateFormFields() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+}
